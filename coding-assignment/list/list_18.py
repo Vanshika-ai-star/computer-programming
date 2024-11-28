a36513f9ab4ac930a4f8b1f966e6c839f1681ba0
Vanshika-ai-star <vanshikaguptatha@gmail.com>
2024-11-28 18:32:30 +0530

    pair with specific element

---
+++ coding-assignment/list/list_18.py
@@ -0,0 +1,12 @@
+#18. Find the Pair with a Specific Sum
+numbers = [2, 4, 3, 7, 1, 9]
+target_sum = 10
+found_pair = False
+for i in range(len(numbers)):
+    for j in range(i + 1, len(numbers)):
+        if numbers[i] + numbers[j] == target_sum:
+            print(f"Pair found: {numbers[i]}, {numbers[j]}")
+            found_pair = True
+            break
+    if found_pair:
+        break
