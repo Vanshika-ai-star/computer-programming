b74ea774197926d29806288d035ac64f27ac9d6f
Vanshika-ai-star <vanshikaguptatha@gmail.com>
2024-11-28 18:24:56 +0530

    sort in descending order

---
+++ coding-assignment/list/list_11.py
@@ -0,0 +1,8 @@
+#11. Sort a List in Descending Order
+
+numbers = [3, 1, 4, 1, 5, 9, 2, 6]
+for i in range(len(numbers)):
+    for j in range(i + 1, len(numbers)):
+        if numbers[i] < numbers[j]:
+            numbers[i], numbers[j] = numbers[j], numbers[i]
+print("Sorted list in descending order:", numbers)
