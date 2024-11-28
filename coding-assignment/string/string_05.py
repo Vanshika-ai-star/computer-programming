fbcadbd189ad15c9cad51f20a8eb4358a400c3cb
Vanshika-ai-star <vanshikaguptatha@gmail.com>
2024-11-28 19:03:42 +0530

    Anagrams

--- coding-assignment/string/string_05.py
+++ coding-assignment/string/string_05.py
@@ -1,10 +1,8 @@
-#Q3: Check if Two Strings are Anagrams
+# Check if Two Strings are Anagrams
 
-# Input strings
+
 s1 = "listen"
 s2 = "silent"
-
-# Check if both strings are anagrams by comparing sorted versions
 if sorted(s1) == sorted(s2):
     print(True) 
 else:
