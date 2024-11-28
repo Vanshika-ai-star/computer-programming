06e198a56d35830531d4f9ee6c93a69086ff9171
Vanshika-ai-star <vanshikaguptatha@gmail.com>
2024-11-28 19:10:22 +0530

    access elements

---
+++ coding-assignment/tuple/tuple_01.py
@@ -0,0 +1,6 @@
+#1. Access Elements in a Tuple
+#Question: Write a program to access and print the second and last elements of a tuple.
+
+t = (10, 20, 30, 40, 50)
+print("Second element:", t[1]) 
+print("Last element:", t[-1])
