4a97a6ccbe6f5d2bc8af76a5b5ae1922d06e500e
Vanshika-ai-star <vanshikaguptatha@gmail.com>
2024-11-28 18:45:14 +0530

    reverse a list

---
+++ coding-assignment/function/function_07.py
@@ -0,0 +1,7 @@
+#7. Write a Function to Reverse a List
+#Question: Create a function to reverse a list.
+
+def reverse_list(lst):
+    return lst[::-1]
+
+print(reverse_list([1, 2, 3, 4, 5]))  # Output: [5, 4, 3, 2, 1]
