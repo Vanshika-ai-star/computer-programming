b0c3fcde29e78fe88e745f78da2b2e9d55bd842b
Vanshika-ai-star <vanshikaguptatha@gmail.com>
2024-11-28 15:04:48 +0530

    uppercase_string

--- coding-assignment/string/string_08.py
+++ coding-assignment/string/string_08.py
@@ -1,4 +1,4 @@
-# 6. Convert a string to uppercase without using the upper() method.
+# Convert a string to uppercase without using the upper() method.
 string = "hello"
 uppercase_string = "".join(chr(ord(char) - 32) if 'a' <= char <= 'z' else char for char in string)
 print(uppercase_string)  
