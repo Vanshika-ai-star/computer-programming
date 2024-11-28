4dbdf6c4e22cf2440ece9a8768b0caa7608d8079
Vanshika-ai-star <vanshikaguptatha@gmail.com>
2024-11-28 18:37:50 +0530

    add two numbers

---
+++ coding-assignment/function/function_01.py
@@ -0,0 +1,7 @@
+#1. Write a Function to Add Two Numbers
+Question: Create a function that takes two numbers and returns their sum.
+
+def add_numbers(a, b):
+    return a + b
+
+print(add_numbers(3, 5)) 
