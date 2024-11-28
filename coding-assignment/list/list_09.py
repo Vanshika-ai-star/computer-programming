ccdac286b1af380263066c05c1fe016f4891562a
Vanshika-ai-star <vanshikaguptatha@gmail.com>
2024-11-28 18:23:18 +0530

    merge two lists

---
+++ coding-assignment/list/list_09.py
@@ -0,0 +1,10 @@
+#9. Merge Two Lists
+
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]
+merged_list = []
+for item in list1:
+    merged_list.append(item)
+for item in list2:
+    merged_list.append(item)
+print("Merged list:", merged_list)
