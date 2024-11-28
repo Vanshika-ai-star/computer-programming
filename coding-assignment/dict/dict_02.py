f17c211f8cd3457b039fb6ed66f0ab07045f8d12
Vanshika-ai-star <vanshikaguptatha@gmail.com>
2024-11-28 19:23:19 +0530

    add a key_value pair

---
+++ coding-assignment/dict/dict_02.py
@@ -0,0 +1,6 @@
+#2. Add a Key-Value Pair to a Dictionary
+#Question: Write a program to add a new key-value pair to a dictionary.
+
+d = {"name": "Alice", "age": 25}
+d["city"] = "New York"
+print("Updated Dictionary:", d) 
