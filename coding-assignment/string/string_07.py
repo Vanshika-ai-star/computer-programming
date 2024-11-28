e138a526418b92c4cdd4cb519511ca56b4223999
Vanshika-ai-star <vanshikaguptatha@gmail.com>
2024-11-28 14:48:58 +0530

    frequency

---
+++ coding-assignment/string/string_07.py
@@ -0,0 +1,9 @@
+# 5. Find the frequency of each character in a string.
+string = "hello"
+frequency = {}
+for char in string:
+    if char in frequency:
+        frequency[char] += 1
+    else:
+        frequency[char] = 1
+print(frequency)  
