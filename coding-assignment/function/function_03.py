020751073c27a9a168bc2415c55171d7367fda43
Vanshika-ai-star <vanshikaguptatha@gmail.com>
2024-11-28 18:40:18 +0530

    prime number

---
+++ coding-assignment/function/function_03.py
@@ -0,0 +1,12 @@
+#3. Write a Function to Check if a Number is Prime
+Question: Create a function that checks if a number is prime.
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+print(is_prime(7))
