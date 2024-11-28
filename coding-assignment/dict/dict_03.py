ef012b7d86dc32c2436e6f4c9db659d1163bb0aa
Vanshika-ai-star <vanshikaguptatha@gmail.com>
2024-11-28 19:24:16 +0530

    update the value

---
+++ coding-assignment/dict/dict_03.py
@@ -0,0 +1,6 @@
+#3. Update the Value of an Existing Key
+#Question: Write a program to update the value of an existing key in a dictionary.
+
+d = {"name": "Alice", "age": 25}
+d["age"] = 30
+print("Updated Dictionary:", d)
