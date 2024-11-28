5c729f8fbaf8487a31df48d2b2110278bbdd1a27
Vanshika-ai-star <vanshikaguptatha@gmail.com>
2024-11-28 18:50:06 +0530

    fabonacci

--- coding-assignment/function/function_09.py
+++ coding-assignment/function/function_09.py
@@ -7,4 +7,4 @@ def fibonacci(n):
         sequence.append(sequence[-1] + sequence[-2])
     return sequence[:n]
 
-print(fibonacci(10))  # Output: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+print(fibonacci(10))  
