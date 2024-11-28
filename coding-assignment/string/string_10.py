44d8490a1ce7225a34ca9296db4681e17d55f237
Vanshika-ai-star <vanshikaguptatha@gmail.com>
2024-11-28 15:04:15 +0530

    vowels_to_star

--- coding-assignment/string/string_10.py
+++ coding-assignment/string/string_10.py
@@ -1,4 +1,4 @@
-# 9. Replace all vowels in a string with '*'.
+# Replace all vowels in a string with '*'.
 string = "hello world"
 vowels = "aeiouAEIOU"
 replaced_string = "".join('*' if char in vowels else char for char in string)
