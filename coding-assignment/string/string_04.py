59b396dd847e0a0f0ed5a22e5eb9e29738743926
Vanshika-ai-star <vanshikaguptatha@gmail.com>
2024-11-28 19:02:55 +0530

    duplicates characters

--- coding-assignment/string/string_04.py
+++ coding-assignment/string/string_04.py
@@ -1,10 +1,5 @@
-#Q1: Remove Duplicate Characters from a String
+# Remove Duplicate Characters from a String
 
-# Input string
 s = "banana"
-
-# Remove duplicates using set and preserve order
 unique_chars = "".join(sorted(set(s), key=s.index))
-
-# Output
 print(unique_chars)  
