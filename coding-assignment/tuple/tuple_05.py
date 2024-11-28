1a4bb59f9b2c1e194fe02eff6db6f7e36a0ff375
Vanshika-ai-star <vanshikaguptatha@gmail.com>
2024-11-28 19:14:26 +0530

    tuple_into_string

---
+++ coding-assignment/tuple/tuple_05.py
@@ -0,0 +1,8 @@
+#5. Convert a Tuple into a String
+#Question: Write a program to convert a tuple of characters into a single string.
+
+t = ('P', 'y', 't', 'h', 'o', 'n')
+result = ""
+for char in t:
+    result += char
+print("String:", result)
